4b82de1224cb7aebd8f878b81114e963d2a03a84
Dennis Nilsson <dennis.nilsson1111@gmail.com>
2018-05-30 16:45:01 +0200

    ParkedVehicle

--- Garage2.0/Models/ParkedVehicle.cs
+++ Garage2.0/Models/ParkedVehicle.cs
@@ -5,8 +5,21 @@ using System.Web;
 
 namespace Garage2._0.Models
 {
-    public class ParkedVehicle
+    public enum VehicleType
     {
+        Car,
+        MotorCycle,
+        Boat,
+        Airplane,
+        Bus
+    }
 
+    public class ParkedVehicle
+    {    
+        public VehicleType Type { get; set; }
+        public String RegistrationNumber { get; set; }
+        public String Color { get; set; }
+        public String Brand { get; set; }
+        public int Wheels { get; set; }
     }
 }
